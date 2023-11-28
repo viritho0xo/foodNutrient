970b748d34f01ec06c19013a2a8400fc2e4f2606
Hoang <vus20007@gmail.com>
2023-11-28 20:37:12 +0700

    CNN & Decision Tree evaluations added, and some minor changes

--- NutrientRandomForest.py
+++ NutrientRandomForest.py
@@ -1,4 +1,4 @@
-import sklearn
+
 import pandas as pd
 from numpy import mean 
 from numpy import std
@@ -27,52 +27,132 @@ def todf(filename):
     data = pd.DataFrame(data, columns=nutrients)
     return data
 
-train = todf("rawFoodDryNutri.txt")
-test = todf("cookedFoodDryNutri.txt")
-y_true = test["Sodium, Na"]
+# train = todf("rawFoodDryNutri.txt")
+# test = todf("cookedFoodDryNutri.txt")
+# y_true = test["Sodium, Na"]
 
-x_train = train
-y_train = train["Sodium, Na"]
+# x_train = train
+# y_train = train["Sodium, Na"]
 
+# scaler = StandardScaler()
+# x_train = scaler.fit_transform(x_train)
+# test = scaler.transform(test)
+# print(test.shape)
 
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-test = scaler.transform(test)
-print(test.shape)
+# x_train = x_train.reshape(-1, 9, 3, 1)
+# test = test.reshape(-1, 9, 3, 1)
+# print(x_train)
+# print(test.shape)
 
-x_train = x_train.reshape(-1, 3, 3, 3)
-test = test.reshape(-1, 3, 3, 3)
-print(test.shape)
+# x_train, x_val, y_train, y_val=train_test_split(x_train,y_train,test_size=0.15)
 
-x_train, x_val, y_train, y_val=train_test_split(x_train,y_train,test_size=0.15)
+# model=Sequential()
 
-model=Sequential()
+# model.add(Conv2D(filters=32,kernel_size=(5,5),padding='Same',activation='relu',input_shape=(9,3,1)))
+# model.add(Conv2D(filters=32,kernel_size=(5,5),padding='Same',activation='relu'))
+# model.add(MaxPool2D(pool_size=(2,2), padding='Same'))
+# model.add(Dropout(0.25))
 
-model.add(Conv2D(filters=32,kernel_size=(5,5),padding='Same',activation='relu',input_shape=(3,3,3)))
-model.add(Conv2D(filters=32,kernel_size=(5,5),padding='Same',activation='relu'))
-model.add(MaxPool2D(pool_size=(2,2), padding='Same'))
-model.add(Dropout(0.25))
+# model.add(Conv2D(filters=64,kernel_size=(3,3),padding='Same',activation='relu'))
+# model.add(Conv2D(filters=64,kernel_size=(3,3),padding='Same',activation='relu'))
+# model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+# model.add(Dropout(0.25))
 
-model.add(Conv2D(filters=64,kernel_size=(3,3),padding='Same',activation='relu'))
-model.add(Conv2D(filters=64,kernel_size=(3,3),padding='Same',activation='relu'))
-model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(128,activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(64,activation='relu'))
+# model.add(Dense(1,activation='linear'))
 
-model.add(Flatten())
-model.add(Dense(128,activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1,activation='linear'))
+# optimizer=RMSprop(learning_rate=0.001,rho=0.9,epsilon=1e-08,decay=0.0)
 
-optimizer=RMSprop(learning_rate=0.001,rho=0.9,epsilon=1e-08,decay=0.0)
+# model.compile(optimizer=optimizer,loss='mean_squared_error',metrics=['mse'])
 
-model.compile(optimizer=optimizer,loss='mean_squared_error',metrics=['mse'])
+# learning_rate_reduction=ReduceLROnPlateau(monitor='val_loss',patience=3,verbose=1,factor=0.5,min_lr=0.00001)
 
-learning_rate_reduction=ReduceLROnPlateau(monitor='val_loss',patience=3,verbose=1,factor=0.5,min_lr=0.00001)
+# epochs = 99
+# batch_size = 5
 
-epochs = 99
-batch_size = 6
+# datagen=ImageDataGenerator(featurewise_center=False, #set input mean to 0 over the data set
+#                           samplewise_center=False, #set each sample mean to 0
+#                           featurewise_std_normalization=False, #divide inputs by std of the data set
+#                           samplewise_std_normalization=False, #divide each input by its std
+#                           zca_whitening=False, #apply ZCA whitening
+#                           rotation_range=10, #randomly rotate images in the range (degrees, 0 to 180)
+#                           zoom_range=0.1, #randomly zoom image
+#                           width_shift_range=0.1, #randomly shift images horizontally (fraction of total width)
+#                           height_shift_range=0.1, #randomly shifts images vertically (fraction of total height)
+#                           horizontal_flip=False, #does not flip images as it could misclassify 6 and 9
+#                           vertical_flip=False) #does not flip images as it could misclassify 6 and 9
+# datagen.fit(x_train)
+# history=model.fit(datagen.flow(x_train,y_train, batch_size=batch_size),
+#                            epochs=epochs, validation_data=(x_val,y_val),
+#                            verbose=2, steps_per_epoch=x_train.shape[0]//batch_size,
+#                            callbacks=[learning_rate_reduction])
+# model.summary()
+# # visualize the structure of CNN
+# from keras.utils import plot_model
+# # plot diagnostic learning curves
+# plt.figure(figsize=(10, 5))
 
-datagen=ImageDataGenerator(featurewise_center=False, #set input mean to 0 over the data set
+# plot loss during training
+# plt.subplot(211)
+# plt.title('Loss')
+# plt.plot(history.history['loss'], label='train')
+# plt.plot(history.history['val_loss'], label='test')
+# plt.legend()
+# # plot mse during training
+# plt.subplot(212)
+# plt.title('Mean Squared Error')
+# plt.plot(history.history['mse'], label='train')
+# plt.plot(history.history['val_mse'], label='test')
+# plt.legend()
+# plt.show()
+# # evaluate the model
+# y_pred = model.predict(test)
+# print(y_pred.shape)
+# # plot the results
+# plt.scatter(y_true, y_pred)
+# plt.xlabel("True Values")
+# plt.ylabel("Predictions")
+# plt.show()
+# MSE = mean_squared_error(y_true, y_pred)
+# MAE = mean_absolute_error(y_true, y_pred)
+# R2 = r2_score(y_true, y_pred)
+# RMSE = MSE ** 0.5
+# SPC = stats.spearmanr(y_true, y_pred)[0]
+# print("MSE: ", MSE, "MAE: ", MAE, "R2: ", R2, "RMSE: ", RMSE, "SPC: ", SPC)
+
+def trainCNN(train, test, label):
+    y_true = test[label]
+    x_train = train
+    y_train = train[label]
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    test = scaler.transform(test)
+    x_train = x_train.reshape(-1, 9, 3, 1)
+    test = test.reshape(-1, 9, 3, 1)
+    x_train, x_val, y_train, y_val=train_test_split(x_train,y_train,test_size=0.15)
+    model=Sequential()
+    model.add(Conv2D(filters=32,kernel_size=(5,5),padding='Same',activation='relu',input_shape=(9,3,1)))
+    model.add(Conv2D(filters=32,kernel_size=(5,5),padding='Same',activation='relu'))
+    model.add(MaxPool2D(pool_size=(2,2), padding='Same'))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(filters=64,kernel_size=(3,3),padding='Same',activation='relu'))
+    model.add(Conv2D(filters=64,kernel_size=(3,3),padding='Same',activation='relu'))
+    model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64,activation='relu'))
+    model.add(Dense(1,activation='linear'))
+    optimizer=RMSprop(learning_rate=0.001,rho=0.9,epsilon=1e-08,decay=0.0)
+    model.compile(optimizer=optimizer,loss='mean_squared_error',metrics=['mse'])
+    learning_rate_reduction=ReduceLROnPlateau(monitor='val_loss',patience=3,verbose=1,factor=0.5,min_lr=0.00001)
+    epochs = 99
+    batch_size = 5
+    datagen=ImageDataGenerator(featurewise_center=False, #set input mean to 0 over the data set
                           samplewise_center=False, #set each sample mean to 0
                           featurewise_std_normalization=False, #divide inputs by std of the data set
                           samplewise_std_normalization=False, #divide each input by its std
@@ -83,42 +163,32 @@ datagen=ImageDataGenerator(featurewise_center=False, #set input mean to 0 over t
                           height_shift_range=0.1, #randomly shifts images vertically (fraction of total height)
                           horizontal_flip=False, #does not flip images as it could misclassify 6 and 9
                           vertical_flip=False) #does not flip images as it could misclassify 6 and 9
-datagen.fit(x_train)
-history=model.fit(datagen.flow(x_train,y_train, batch_size=batch_size),
-                           epochs=epochs, validation_data=(x_val,y_val),
-                           verbose=2, steps_per_epoch=x_train.shape[0]//batch_size,
-                           callbacks=[learning_rate_reduction])
+    datagen.fit(x_train)
+    history=model.fit(datagen.flow(x_train,y_train, batch_size=batch_size),
+                            epochs=epochs, validation_data=(x_val,y_val),
+                            verbose=2, steps_per_epoch=x_train.shape[0]//batch_size,
+                            callbacks=[learning_rate_reduction])
+    return model, y_true, test
 
-# plot loss during training
-plt.subplot(211)
-plt.title('Loss')
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='test')
-plt.legend()
-# plot mse during training
-plt.subplot(212)
-plt.title('Mean Squared Error')
-plt.plot(history.history['mse'], label='train')
-plt.plot(history.history['val_mse'], label='test')
-plt.legend()
-plt.show()
-# evaluate the model
-y_pred = model.predict(test)
-print(y_pred.shape)
-# plot the results
-plt.scatter(y_true, y_pred)
-plt.xlabel("True Values")
-plt.ylabel("Predictions")
-plt.show()
-MSE = mean_squared_error(y_true, y_pred)
-MAE = mean_absolute_error(y_true, y_pred)
-R2 = r2_score(y_true, y_pred)
-RMSE = MSE ** 0.5
-SPC = stats.spearmanr(y_true, y_pred)[0]
-print("MSE: ", MSE, "MAE: ", MAE, "R2: ", R2, "RMSE: ", RMSE, "SPC: ", SPC)
+def toSpreadSheet(raw, cooked, name):
+    data = []
+    for label in nutrients:
+        model = trainCNN(todf(raw+ ".txt"), todf(cooked + ".txt"), label)
+        y_true = model[1]
+        test = model[2]
+        y_pred = model[0].predict(test)
+        MSE = mean_squared_error(y_true, y_pred)
+        MAE = mean_absolute_error(y_true, y_pred)
+        R2 = r2_score(y_true, y_pred)
+        RMSE = MSE ** 0.5
+        SPC = stats.spearmanr(y_true, y_pred)[0]
+        data.append([label, MSE, MAE, R2, RMSE, SPC])
+    data = pd.DataFrame(data, columns=["Nutrient", "MSE", "MAE", "R2", "RMSE", "SPC"])
+    data.to_csv("Evaluations/"+ name + "CNN.csv", index=False)  
 
-
-
+#toSpreadSheet("rawFoodDryNutri", "cookedFoodDryNutri", "dryFood")
+toSpreadSheet("rawFoodWetNutri", "cookedFoodWetNutri", "wetFood")
+    
 
 # split data into train and test sets
 # X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.33, random_state=42)
